ac222d28b7945be2799e0744604aab950bcb6715
AndJav <1962264482@qq.com>
2019-07-15 14:14:08 +0800

    首次提交

---
+++ producer/src/test/java/TestTopic_Producer.java
@@ -0,0 +1,19 @@
+import com.pyg.TopicProducer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext-jms-producer.xml")
+public class TestTopic_Producer {
+    @Autowired
+    private TopicProducer topicProducer;
+
+    @Test
+    public void sendTextQueue() {
+        topicProducer.sendTextMessage("topic send message 。。。。。。");
+    }
+}
